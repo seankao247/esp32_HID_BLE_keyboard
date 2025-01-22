0d3c04792fc56d8a5e3f6e4b54a94029a5d22dcb
seankao247 <seankao2010@gmail.com>
2025-01-22 13:43:34 +0800

    數字按鍵功能成功，目前先用3鍵

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,27 @@
 
 BleKeyboard bleKeyboard;
 
+const int buttonPins[10] = {2, 4, 5};  // 10 個按鍵對應 GPIO
+//2, 4, 5, 18, 19, 21, 22, 23, 25, 26
 void setup() {
     Serial.begin(115200);
     bleKeyboard.begin();  // 初始化 BLE 鍵盤
-    Serial.println("BLE Keyboard Initialized");
+    for (int i = 0; i < 3; i++) {
+        pinMode(buttonPins[i], INPUT_PULLUP);  // 按鍵為 INPUT_PULLUP 模式
+    }
 }
 
 void loop() {
-    if (bleKeyboard.isConnected()) {  // 確認是否連接到主機
-        Serial.println("Sending 'Hello World'");
-        bleKeyboard.print("Hello World!");  // 傳送字串到主機
-        delay(1000);  // 每秒傳送一次
+    if (bleKeyboard.isConnected()) {  // 確認連線狀態
+                for (int i = 0; i < 3; i++) {
+            if (digitalRead(buttonPins[i]) == LOW) {  // 檢測按鍵是否按下
+                bleKeyboard.print(String(i));  // 傳送對應數字
+                delay(500);  // 避免重複輸入
+            }
+        }
+    }
+    else
+    {
+        Serial.println("not connected");
     }
 }
